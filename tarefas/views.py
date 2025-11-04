a6b3dcadd2d1241c9679c986215c5c3ced3ca5db
Estudante Nobre <trademilionario99@gmail.com>
2025-11-04 17:37:28 -0300

    Add files via upload

---
+++ tarefas/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from.models import Task
+
+
+def home(request):
+    tasks = Task.objects.all()
+    completed_tasks = len(tasks)
+    total_tasks = 0
+    remaining_tasks = 0
+    pct = 0
+
+    return render(
+        request,
+        'tarefas/home.html',
+          {
+              'tasks' : tasks,
+              'total' : total_tasks,
+              'completed' : completed_tasks,
+              'remaning' : remaining_tasks,
+              'pct' : pct
+            
+          })
