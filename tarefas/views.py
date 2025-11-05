83a6c9038c205782100516ae3aa260d922ed491b
Estudante Nobre <trademilionario99@gmail.com>
2025-11-05 17:09:37 -0300

    Add files via upload

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from.models import Task
-
+from .forms import TaskForm
 
 def home(request):
     tasks = Task.objects.all()
-    completed_tasks = len(tasks)
-    total_tasks = 0
+    completed_tasks = 0
+    total_tasks = len(tasks)
     remaining_tasks = 0
     pct = 0
+    
+    #Contando quab=ntas tarefas existem
+    for task in tasks:
+        if task.done == True:
+            completed_tasks += 1
 
+    #Contar quantas tarefas ainda falta
+    remaning_task = total_tasks - completed_tasks
+    
+    #Calculando porcentagem
+    if total_tasks > 0:
+        pct = int((completed_tasks / total_tasks)*100)
+        
     return render(
         request,
         'tarefas/home.html',
@@ -20,3 +32,25 @@ def home(request):
               'pct' : pct
             
           })
+    
+def add(request):
+    form = TaskForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return redirect('home')
+
+def toggle(reqwuest, id):
+    task = get_object_or_404(Task, id=id)
+    
+    if task.done == True:
+        task.done = False
+    else:
+        task.done = True
+        
+    task.save()
+    return redirect('home')
+
+def delete(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.delete()
+    return redirect('home')
